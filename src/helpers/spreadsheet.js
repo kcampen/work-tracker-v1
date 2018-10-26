62a8efb64a81b055fd6277d630e7b99f89024c79
kcampen <katycampen@gmail.com>
2018-10-26 16:01:49 -0500

    updating app to include cards filters and styles

--- src/helpers/spreadsheet.js
+++ src/helpers/spreadsheet.js
@@ -1,3 +1,7 @@
+// import { orderBy } from "lodash";
+// import { hash } from "./utils";
+// import { get } from "./localStorage";
+
 import config from "../config";
 
 /**
@@ -9,17 +13,36 @@ export function load(callback) {
     window.gapi.client.sheets.spreadsheets.values
       .get({
         spreadsheetId: config.spreadsheetId,
-        range: "Sheet1!A4:T"
+        range: "Sheet1!A4:T",
+        valueRenderOption: "UNFORMATTED_VALUE"
       })
       .then(
         response => {
           const data = response.result.values;
-          const cars =
+          let cars =
             data.map(car => ({
               year: car[0],
               make: car[1],
-              model: car[2]
+              model: car[2],
+              name: `${car[0]} ${car[1]} ${car[2]}`,
+              wStyling: car[3],
+              wAccel: car[4],
+              wHandling: car[5],
+              wFun: car[6],
+              wCool: car[7],
+              wTotal: car[8],
+              dFeatures: car[9],
+              dComfort: car[10],
+              dQuality: car[11],
+              dPracticality: car[12],
+              dValue: car[13],
+              dTotal: car[14],
+              score: car[15],
+              filmCity: car[17],
+              filmState: car[18],
+              carCountry: car[19]
             })) || [];
+
           callback({
             cars
           });
